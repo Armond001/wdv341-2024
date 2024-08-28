080f3a1c8ba748f6fe2d7e94fa6031779d032cda
Armond001 <Armond0621@gmail.com>
2024-08-28 10:22:42 -0500

    wdv341 homework page

---
+++ wdv341.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>PHP homework</title>
+    <meta name="keywords" content=" jacob, web development homework">
+    <meta name="Author:" value="Jacob Scroggins">
+    <meta name="destription" content="homework ">
+    <meta charset="UTF-8">
+    <meta name="viewprt" content="width=device-width, initial-scale=1.0">
+
+    <style>
+        body {
+            background-color: rgb(50, 43, 98);
+            color: white;
+            font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+
+
+        }
+
+        header {
+            font-family: 'Quicksand', sans-serif;
+
+            align-items: center;
+            text-align: center;
+
+        }
+
+        .row {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+            columns: 3;
+        }
+
+        figure {
+            width: 25%;
+            height: 20%;
+        }
+
+        figcaption {
+            background-color: rgb(130, 116, 222);
+            text-align: center;
+            padding: 2%;
+            border-radius: 48px;
+            height: 50%;
+            width: 30%;
+            margin: auto;
+            box-shadow: 3px 2px 23px -1px black;
+
+        }
+    </style>
+
+    <script>
+
+    </script>
+
+</head>
+
+<body>
+
+    <header>
+        <h1>WDV341 Intro to PHP homework</h1>
+    </header>
+
+    <main>
+        <div class="row">
+
+        </div>
+    </main>
+
+
+
+
+</body>
+
+</html>
